7ac740bd12fc0cbdcfc1810a68fe2071bbc2da00
Hyunmin Shin <shm1193@gmail.com>
2019-07-05 00:32:38 +0900

    (name changed)

---
+++ [1009] 분산처리.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,a,n) for (int i=a;i<n;i++)
+#define per(i,a,n) for (int i=n-1;i>=a;i--)
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(),(x).end()
+#define fi first
+#define se second
+#define sz(x) ((int)(x).size())
+typedef vector<int> VI;
+typedef long long ll;
+typedef pair<int,int> PII;
+const ll mod=1000000007;
+ll powmod(ll a,ll b) {ll res=1;a%=mod; assert(b>=0); for(;b;b>>=1){if(b&1)res=res*a%mod;a=a*a%mod;}return res;}
+ll gcd(ll a,ll b) { return b?gcd(b,a%b):a;}
+// head
+
+int main() {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+    int q;
+    cin >> q;
+    while(q--) {
+        int n = 1, a, b;
+        cin >> a >> b;
+        a %= 10;
+        if(a != 1)
+            rep(i, 0, b) n = (n * a) % 10;
+        if(n != 0) cout << n << '\n';
+        else cout << 10 << '\n';
+    }
+}
