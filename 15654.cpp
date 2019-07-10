026d5f91466d53b8905412bf5ae7d2529f3712ec
Hyunmin Shin <shm1193@gmail.com>
2019-07-10 22:25:18 +0900

    [BOJ] 15654번: N과 M (5)

---
+++ 15654.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,a,n) for (int i=a;i<n;++i)
+#define per(i,a,n) for (int i=n-1;i>=a;--i)
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(),(x).end()
+#define fi first
+#define se second
+#define sz(x) ((int)(x).size())
+typedef vector<int> VI;
+typedef long long ll;
+typedef pair<int,int> PII;
+const ll mod=1000000007;
+ll powmod(ll a,ll b) {ll res=1;a%=mod; assert(b>=0); for(;b;b>>=1){if(b&1)res=res*a%mod;a=a*a%mod;}return res;}
+ll gcd(ll a,ll b) { return b?gcd(b,a%b):a;}
+// head
+
+int n, m, d[10];
+bool visited[10];
+VI v;
+
+void dfs() {
+    if(sz(v) == m) {
+        rep(i, 0, m) cout << v[i] << ' ';
+        cout << '\n';
+        return;
+    }
+    
+    rep(i, 1, n+1) {
+    	if(!visited[i]) {
+	        visited[i] = true; v.pb(d[i]);
+	        dfs();
+	        visited[i] = false; v.pop_back();
+    	}
+    }
+}
+
+int main() {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+    cin >> n >> m;
+    rep(i, 1, n+1) cin >> d[i];
+    sort(d + 1, d + n + 1);
+    dfs();
+}
